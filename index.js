dfa3af92e773404ef8f7d774bca34e95a6236aa5
aloktiwari082013-afk <aloktiwari082013@gmail.com>
2025-12-17 10:43:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,10 +22,10 @@ setInterval(() => {
 // U CAN ONLY EDIT THIS SECTION!!
 function createBot () {
 const bot = mineflayer.createBot({
-  host: 'crazymcsmp.aternos.me', 
+  host: 'crazymcsmp_.aternos.me', 
   version: false, // U can replace with 1.16.5 for example, remember to use ', = '1.16.5'
   username:'oppika', 
-  port: 60004,
+  port: 63172,
   plugins: [AutoAuth],
   AutoAuth: 'bot112022'
 })
